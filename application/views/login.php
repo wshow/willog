b140071eab60e09e3058e199d56767e113e14448
王之琳 <willin@willin.org>
2013-08-06 00:08:03 +0800

    Login Page

--- application/views/login.php
+++ application/views/login.php
@@ -1,22 +1,36 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
 ?><!doctype html>
-<!--[if !IE]>      <html class="no-js non-ie" lang="zh-CN"> <![endif]-->
-<!--[if IE 7 ]>    <html class="no-js ie7" lang="zh-CN"> <![endif]-->
-<!--[if IE 8 ]>    <html class="no-js ie8" lang="zh-CN"> <![endif]-->
-<!--[if IE 9 ]>    <html class="no-js ie9" lang="zh-CN"> <![endif]-->
-<!--[if gt IE 9]><!--> <html class="no-js" lang="zh-CN"> <!--<![endif]-->
+<!--[if lt IE 9 ]><html class="no-js ie ie6" lang="zh-CN"> <![endif]-->
+<!--[if gte IE 9 ]><html class="no-js ie ie9" lang="zh-CN"> <![endif]-->
+<!--[if !IE]><!--><html class="no-js" lang="zh-CN"> <!--<![endif]-->
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= ($title?$title.' - ':' ').$site_name.' - Powered by Willog' ?></title>
+    <link rel="stylesheet" href="<?=base_url('/assets/css/bootstrap.min.css')?>">
 </head>
 <body>
-    <?php
-    echo $this->options->get('site_name','cn');
-    echo '<hr>';
-    var_dump($this->options->get());
-    //code.jquery.com/jquery.min.js
-    ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-lg-offset-3">...</div>
+        </div>
+        <form class="form-signin">
+            <h2 class="form-signin-heading">Please sign in</h2>
+            <input type="text" class="input-block-level" placeholder="Email address" autofocus>
+            <input type="password" class="input-block-level" placeholder="Password">
+            <label class="checkbox">
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
+            <button class="btn btn-large btn-primary btn-block" type="submit">Sign in</button>
+        </form>
+
+    </div> <!-- /container -->
+
+    <div class="scripts">
+        <script type="text/javascript" src="//code.jquery.com/jquery.min.js"></script>
+        <script type="text/javascript" src="<?=base_url('/assets/js/responsive-modernizr.js')?>"></script>
+        <script type="text/javascript">window.jQuery || document.write('<script src="<?=base_url('/assets/js/jquery-1.10.2.min.js')?>">\x3C/script>')</script>
+    </div>
 </body>
 </html>
