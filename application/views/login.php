baeb7d85a7113ad23cac8dbc94284494e7cf3829
王之琳 <willin@willin.org>
2013-08-07 14:55:08 +0800

    Foundation

--- application/views/login.php
+++ application/views/login.php
@@ -13,18 +13,36 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-lg-6 col-lg-offset-3">...</div>
+            <div class="text-center col-lg-6 col-lg-offset-3">
+                <h1 class="logo">Willog</h1>
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        <label for="inputEmail" class="col-lg-2 control-label">Email</label>
+                        <div class="col-lg-10">
+                            <input type="text" class="form-control" id="inputEmail" placeholder="Email">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword" class="col-lg-2 control-label">Password</label>
+                        <div class="col-lg-10">
+                            <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                        </div>
+                    </div>
+                    <div class="form-group text-left">
+                        <div class="col-lg-offset-2 col-lg-10">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox"> Remember me
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                            <input type="submit" class="btn btn-primary btn-lg btn-block" value=" Login " >
+                    </div>
+                </form>
+            </div>
         </div>
-        <form class="form-signin">
-            <h2 class="form-signin-heading">Please sign in</h2>
-            <input type="text" class="input-block-level" placeholder="Email address" autofocus>
-            <input type="password" class="input-block-level" placeholder="Password">
-            <label class="checkbox">
-                <input type="checkbox" value="remember-me"> Remember me
-            </label>
-            <button class="btn btn-large btn-primary btn-block" type="submit">Sign in</button>
-        </form>
-
     </div> <!-- /container -->
 
     <div class="scripts">
